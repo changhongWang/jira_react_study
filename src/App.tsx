6c86f18e4abfe3c4a181ef960b928046d8568a7e
changhong <changhongwang@126.com>
2021-11-16 12:15:45 +0800

    feat: 增加错误边界 errorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import AuthenticatedApp from "./authenticated-app";
 import UnAuthenticatedApp from "./unauthenticated-app";
 import { useAuth } from "./context/auth-context";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 import "./App.css";
+import { FullPageErrorFallback } from "./components/lib";
 
 function App() {
   const { user } = useAuth();
   return (
     <div className="App">
-      {user ? <AuthenticatedApp /> : <UnAuthenticatedApp />}
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        {user ? <AuthenticatedApp /> : <UnAuthenticatedApp />}
+      </ErrorBoundary>
     </div>
   );
 }
--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -8,11 +8,17 @@ export class ErrorBoundary extends React.Component<
 > {
   state = { error: null };
 
-  static getDerivedStateFromError(e: Error) {
-    return { e };
+  static getDerivedStateFromError(error: Error) {
+    return { error };
   }
 
   render() {
-    return <div></div>;
+    const { error } = this.state;
+    const { fallbackRender, children } = this.props;
+    if (error) {
+      return fallbackRender({ error });
+    }
+    // 无异常情况下，返回包裹的children
+    return children;
   }
 }
