6e6f415dbe20ef3a51ee775f3168630ffd15d2c3
changhong <changhongwang@126.com>
2021-12-14 23:52:38 +0800

    feat: 自动化测试

---
+++ src/__tests__/useAsync.ts
@@ -0,0 +1,63 @@
+// 测试hooks
+import { useAsync } from "../utils/useAsync";
+import { renderHook } from "@testing-library/react-hooks";
+import { act } from "react-dom/test-utils";
+
+const defaultState: ReturnType<typeof useAsync> = {
+  stat: "idle",
+  data: null,
+  error: null,
+
+  isIdle: true,
+  isLoading: false,
+  isError: false,
+  isSuccess: false,
+
+  // expect.any(Function) 不是很重要的一个函数
+  run: expect.any(Function),
+  setData: expect.any(Function),
+  setError: expect.any(Function),
+  retry: expect.any(Function),
+  setRetry: expect.any(Function),
+};
+
+const loadingState: ReturnType<typeof useAsync> = {
+  ...defaultState,
+  stat: "loading",
+  isIdle: false,
+  isLoading: true,
+};
+
+const successState: ReturnType<typeof useAsync> = {
+  ...defaultState,
+  stat: "success",
+  isIdle: false,
+  isSuccess: true,
+};
+
+test("useAsync 可以异步处理", async () => {
+  let resolve: any, reject;
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+  const { result } = renderHook(() => useAsync());
+  expect(result.current).toEqual(defaultState);
+
+  let p: Promise<any>;
+  act(() => {
+    p = result.current.run(promise);
+  });
+  expect(result.current).toEqual(loadingState);
+
+  const resolvedValue = { mockedValue: "resolved" };
+  await act(async () => {
+    resolve(resolvedValue);
+    await p;
+  });
+  expect(result.current).toEqual({
+    ...successState,
+    data: resolvedValue,
+  });
+});
